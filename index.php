02a2e254059e27ce049681684ea4e5eb5279cfbd
anangsproject <0001mandraguna@gmail.com>
2021-06-11 07:29:45 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+define("phi",3.14);
+
+$r=5;
+$luas=phi*($r*$r);
+
+echo "Hasil dari luas lingkaran :" . "<br/>";
+echo "Jari-jari =" . $r . "<br/>";
+echo "Nilai phi =" . phi . "<br/>";
+echo "Rumus = phi x (r x r)" . "<br/>";
+echo "Jadi luas lingkaran adalah =" . $luas;
+
+?>
